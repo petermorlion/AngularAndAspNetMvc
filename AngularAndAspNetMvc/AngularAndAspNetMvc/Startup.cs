f382e0aa00c5abbbcfe339bef5c8536c53c66f0f
petermorlion <peter.morlion@live.be>
2014-07-15 19:46:42 +0200

    New ASP.NET MVC application from template

---
+++ AngularAndAspNetMvc/AngularAndAspNetMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularAndAspNetMvc.Startup))]
+namespace AngularAndAspNetMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
